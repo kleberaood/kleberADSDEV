e5708c806bdb931539fc6fd49d8b58b540650873
04035476 <kleber.aood@hotmail.com>
2019-02-21 20:23:14 -0300

    exemplos aula

--- src/Exemplo/Catalogo.java
+++ src/Exemplo/Catalogo.java
@@ -6,9 +6,34 @@ public class Catalogo {
         int a= 10, b= 90, c= -100;
         if (a>0){
              a = c;
+             if (c > a ){
+                 c = a;
+             }
 
         }
         System.out.println(a);
-    }
-}
+        if (c > a) {
+            c = a;
+        } else {
+            c = b;
+        }
+        System.out.println(c);
 
+        /*switch/*
+        int op = 3;
+        int v = 0;
+        switch ( op ){
+          case 1:
+            v = 1;
+          break;
+          case 2:
+            v = 2;
+          break;
+          default:
+            v = -1;
+
+    }
+         System.out.println(V);
+
+    }
+     }
